050fda0b5b8f9f74cffe498a77f66ca5d37fe643
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-21 12:42:18 +0100

    add comment milestone 2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 Milestone 1
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
 */
 
 const app = new Vue (
@@ -93,7 +97,8 @@ const app = new Vue (
                     ],
                 },
             ],
-            counter: 0
+            counter: 0,
+            searchText: '',
         },
         methods: {
             getLastDate: function (index) {
